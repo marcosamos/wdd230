3a09eae211b1005b9fed73472b3d35fd3486d389
mrus <mrus.corporation@gmail.com>
2022-03-02 15:00:05 -0800

    update

--- chamber/js/bussines.js
+++ chamber/js/bussines.js
@@ -1,1 +1,39 @@
-const requestUrl = ""
+const requestUrl = "https://marcosamos.github.io/wdd230/chamber/data/data.json"
+const cards = document.querySelector(".directory");
+
+fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const business = jsonObject['business'];
+    business.forEach(displayBusiness);
+  });
+
+  function displayBusiness(business){
+    let card = document.createElement("section");
+    let companyName = document.createElement("h2");
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
+
+    logo.setAttribute('src', business.imageurl);
+    logo.setAttribute('alt', `Logo of  ${business.name}`);
+    logo.setAttribute('loading', 'lazy');
+
+    companyName.textContent = business.name;
+    address.textContent = business.address;
+    phone.textContent = business.phone;
+    website.textContent = business.website;
+
+
+    card.appendChild(companyName);
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+
+    cards.appendChild(card);
+  }
