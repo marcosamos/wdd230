b71ed2f6b203428e0aad876628558a064ffb914a
mrus <mrus.corporation@gmail.com>
2022-03-07 12:23:54 -0800

    weather ipa

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=dd3df6bf460d18fe6d33bc4f08d44723";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  });
