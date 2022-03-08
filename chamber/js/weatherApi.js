acdf1e0677e4a6ca3d23bae7fa984d75c42f35e8
mrus <mrus.corporation@gmail.com>
2022-03-08 15:34:14 -0800

    add changes

---
+++ chamber/js/weatherApi.js
@@ -0,0 +1,14 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Tijuana&units=imperial&APPID=dd3df6bf460d18fe6d33bc4f08d44723";
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) => {
+console.log(jsObject);
+const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+document.querySelector('#weathericon').setAttribute('src', iconsrc);
+document.querySelector('.temperature').textContent = Math.round(jsObject.main.temp);
+document.querySelector('#weathericon').setAttribute('alt', desc);
+document.querySelector("#desc").textContent = desc
+document.querySelector('.windSped').textContent = jsObject.wind.speed;
+
+});
