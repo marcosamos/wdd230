5596e03ced11ebcc27ea14f1adaf8754f872743f
mrus <mrus.corporation@gmail.com>
2022-03-10 07:15:40 -0800

    changes

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -4,9 +4,10 @@ const wspeed = document.querySelector(".windSped");
 
 if (temp >= 50 && wspeed > 3){
     const windChill= Math.round((35.74 + 0.6215 * temp - 35.75 * wspeed ** 0.16 + 0.4275 * temp * wspeed ** 0.16) * 100 / 100);
-    const roundedWindChill = Math.round(windChill);
-    document.querySelector(".windChill").textContent = roundedWindChill;
-}   else {
+
+    document.querySelector(".windChill").textContent = windChill;
+}   
+else {
     document.querySelector(".windChill").textContent = "N/A";
 
 }
